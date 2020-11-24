53cb57289194bcb28490041bf9e46e318d11a062
floryhyy <38958428+floryhyy@users.noreply.github.com>
2020-11-24 02:25:43 -0800

    update data.py

--- src/data/data.py
+++ src/data/data.py
@@ -13,6 +13,7 @@ def get_data(ontology,mesh_file,reddit_names):
     author_ids = []
     for t in filenames:
         filepath = '/teams/DSC180A_FA20_A00/a07group01/data/' + t + '.json'
+        print(filepath)
         df = pd.read_json(filepath)
         all_text.extend(list(df['text']))
         author_ids.extend(list(df.index))
