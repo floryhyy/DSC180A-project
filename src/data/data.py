bbbe12c898e807fe444a31f3b6cbe3eb6629a4a2
floryhyy <38958428+floryhyy@users.noreply.github.com>
2020-11-24 02:22:39 -0800

    Update data.py

--- src/data/data.py
+++ src/data/data.py
@@ -1,7 +1,7 @@
 import pandas as pd
 def get_data(ontology,mesh_file,reddit_names):
     on=pd.read_csv(ontology,usecols=['opioid1'])
-    mesh=pd.read_csv('MESH.csv',usecols=['Preferred Label'])
+    mesh=pd.read_csv(mesh_file,usecols=['Preferred Label'])
     mesh_ls = list(set(mesh['Preferred Label']))
     mesh_terms = list(set([i.lower() for i in mesh_ls]))
     on_ls = [i.strip('@en').strip('"') for i in list(set(on['opioid1']))]
