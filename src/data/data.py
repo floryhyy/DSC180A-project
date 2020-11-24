705fd5230b9c1602c3fceff847bca9f50284228c
floryhyy <38958428+floryhyy@users.noreply.github.com>
2020-11-24 03:40:39 -0800

    Update data.py

--- src/data/data.py
+++ src/data/data.py
@@ -10,7 +10,7 @@ def get_data(ontology,mesh_file,reddit_names):
     all_text = []
     author_ids = []
     if '.json' in reddit_names:
-        df = pd.read_json(reddit_names)
+        df = pd.read_json(reddit_names,orient='index')
         print(df)
         all_text.extend(list(df['text']))
         author_ids.extend(list(df.index))
