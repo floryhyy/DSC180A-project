5f4b1ce4b6c3a4efc0720c62000dfd23877b383c
floryhyy <38958428+floryhyy@users.noreply.github.com>
2020-11-23 21:45:04 -0800

    Update format_data.py

--- src/data/format_data.py
+++ src/data/format_data.py
@@ -2,27 +2,24 @@ import nltk
 import nltk.data
 import pandas as pd
 import re
-def make_content(search_terms, text_df, textcol_name):
+def make_content_all(search_terms, text, authors):
     #Initialize a dictionary
     content = dict()
-
+    
     #Add search terms to dictionary with numbered key
     for i in range(len(search_terms)):
         if len(search_terms[i]) > 3:
-            content[i] = search_terms[i]
-
-    min_length = min(map(len, search_terms))
-
+            content[i] = search_terms[i].strip().lower()
+                
     #Add text to dictionary with author as key
-    for index, row in text_df.iterrows():
+    for i in range(len(text)):
         key_n = 0
-        for sentence in row[textcol_name].split('\n'):
-            if len(sentence) > 3:
-                key = index + "_" + str(key_n)
-                content[key] = sentence.strip('\s+').replace(',',' ').lower()
+        for sentence in text[i].split():
+            s=sentence.replace(',',' ').strip().lower()
+            if len(s) > 5:
+                key = authors[i] + "_" + str(key_n)
+                content[key] = s
                 key_n += 1
-            #elif len(sentence.split())>1: 
-             #   print(len(sentence),sentence)
     return content
 
 def text_to_list (text):
